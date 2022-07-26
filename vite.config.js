ca487a2682df0ad86395cc4665b6f4ec11f92585
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-07-25 22:01:29 -0300

    feat: add host

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,11 @@ export default defineConfig(({ command, mode }) => {
       }),
     ],
     server: {
+      host: "0.0.0.0",
       port: 3000,
     },
     preview: {
+      host: "0.0.0.0",
       port: 8080,
     },
     define: {
